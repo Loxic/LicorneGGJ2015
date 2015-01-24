ab0f695112061376cb5f401a7197460a28a95ec0
chaillous <mathieu_kendo@hotmail.fr>
2015-01-24 21:48:24 +0100

    parsing, et dialogues

---
+++ Assets/Scripts/UI/Dialogues/GetText.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GetText : MonoBehaviour {
+
+	public int dialNum;
+	private XMLParsing xmlParse;
+	string dialogue;
+	Text textPrint;
+	private GameObject dialogueHandler;
+
+
+	public void Start(){
+		//dialogueHandler = GameObject.Find ("DialogueHandler");
+		textPrint = GetComponent<Text>();
+		//xmlParse = dialogueHandler.GetComponent<XMLParsing>();
+		dialogue = XMLParsing.getDialogue(dialNum);
+		textPrint.text = dialogue;
+	}
+
+
+
+}
---
+++ Assets/Scripts/UI/Dialogues/XMLParsing.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+using System;
+
+
+public abstract class XMLParsing {
+	private static XmlDocument xmlDoc;
+	private static string fileName="./Annexe/Dialogues.xml";
+	
+
+	public static String getDialogue(int dialNum) {
+		xmlDoc = new XmlDocument ();
+		xmlDoc.Load(fileName);
+		return ParseDialogue(xmlDoc.SelectNodes("dials/dial"), dialNum);
+		
+		}
+
+
+	public static String ParseDialogue(XmlNodeList nodes, int dialNum){
+		foreach (XmlNode node in nodes) {
+			if (Convert.ToInt32 (node.Attributes.GetNamedItem ("num").Value) == dialNum) {
+				return node.InnerText;
+			}
+	
+		}
+
+		return null;
+	}
+
+
+}
