5edf175a6e67d0a87859ecfa23597eb9d279445a
Loxic <a.blazart@gmail.com>
2015-01-24 01:10:15 +0100

    Ajout scène simple de menu principal

---
+++ Assets/Scripts/UI/Settings.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Settings : MonoBehaviour {
+
+	bool invertY = false;
+
+
+	public void invertMouse(){
+		invertY = true;
+	}
+
+	public void qwerty(){
+
+
+	}
+
+	public void azerty(){
+
+
+
+	}
+
+
+
+}
