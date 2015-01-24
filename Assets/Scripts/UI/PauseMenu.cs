9050147b4f0a77baae60ed4999e03001c3a2851e
Loxic <a.blazart@gmail.com>
2015-01-24 15:18:45 +0100

    Main menu script
    Added some UI scripts (Main-menu, Pause-menu),

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+	private bool settingsUp;
+	private bool pauseUp;
+	private GameObject pause;
+	private GameObject settings;
+
+
+
+	void Awake () {
+		//need to the object to be activate on beginning
+		pause = GameObject.Find ("PauseCanvas");
+		settings = GameObject.Find ("SettingsCanvas");
+
+	}
+
+	void Start () {
+		//Deactivate the menu
+		settings.SetActive (false);
+		pause.SetActive (false);
+		}
+	
+	// Update is called once per frame
+	void Update () {
+		//actualize booleans 
+		pauseUp = pause.activeSelf;
+		settingsUp = settings.activeSelf;
+
+		//Differents behaviour
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if (!pauseUp && !settingsUp){
+				pause.SetActive(true);
+			}
+		
+			else if(pauseUp && !settingsUp){
+				pause.SetActive(false);
+			}
+
+			else if(!pauseUp && settingsUp){
+				settings.SetActive(false);
+				pause.SetActive(true);
+			}
+
+
+		}
+	
+	}
+}
