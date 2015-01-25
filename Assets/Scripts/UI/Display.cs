55954ae2c89f8bc59b84fa5c73583aff68d02ea5
Loxic <a.blazart@gmail.com>
2015-01-25 01:09:54 +0100

    Ajout Display pour les ressources

---
+++ Assets/Scripts/UI/Display.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Display : MonoBehaviour {
+
+	private GameObject displayCanvas;
+	private Text textPrint;
+
+	void Awake (){
+		displayCanvas = GameObject.Find ("DisplayCanvas");
+		textPrint = GetComponent<Text> ();
+		}
+
+	void Start (){
+		displayCanvas.SetActive (false);
+	}
+
+	public void DisplayWood(int nb){
+		displayCanvas.SetActive (true);
+		textPrint.text = "Remaining wood : \n" + nb;
+	}
+
+	public void DisplayFood(int nb){
+		displayCanvas.SetActive (true);
+		textPrint.text = "Remaining food : \n" + nb;
+	}
+
+
+
+	public void EndDisplay(){
+		displayCanvas.SetActive (false);
+	}
+	
+}
